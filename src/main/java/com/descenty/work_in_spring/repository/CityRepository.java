938551b68fbd44da901a49e1adce49271cba9130
Deverty <bychenkov.a.k@edu.mirea.ru>
2023-07-11 05:53:36 +0300

    add spring data rest and docker images building using jib

--- src/main/java/com/descenty/work_in_spring/repository/CityRepository.java
+++ src/main/java/com/descenty/work_in_spring/repository/CityRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.descenty.work_in_spring.entity.City;
 
+@RepositoryRestResource(path = "city")
 public interface CityRepository extends JpaRepository<City, Long> {
     @Query("SELECT c FROM City c WHERE ?1 is null or c.name LIKE %?1%")
     List<City> findAll(String name);
--- src/main/java/com/descenty/work_in_spring/repository/VacancyRepository.java
+++ src/main/java/com/descenty/work_in_spring/repository/VacancyRepository.java
@@ -3,6 +3,8 @@ package com.descenty.work_in_spring.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.descenty.work_in_spring.entity.Vacancy;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "vacancy")
 public interface VacancyRepository extends JpaRepository<Vacancy, Long> {
 }
